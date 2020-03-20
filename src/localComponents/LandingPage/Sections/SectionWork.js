9133c175d83170ba5a09b72033a0537db3cf544d
Salman Ahmed <salman@hileets.com>
2020-03-20 11:22:54 +0500

    wip

--- src/localComponents/LandingPage/Sections/SectionWork.js
+++ src/localComponents/LandingPage/Sections/SectionWork.js
@@ -17,30 +17,15 @@ import workStyle from 'assets/jss/material-kit-pro-react/views/landingPageSectio
 const useStyles = makeStyles(workStyle)
 
 export default function SectionWork() {
-  const [fire, setFire] = React.useState(null)
-  // useEffect(() => {
-  //   const firebaseConfig = {
-  //     apiKey: 'AIzaSyBkdSktw-ORhHkPdDyptnHfyvHdNsN3Uys',
-  //     authDomain: 'vikor-ce726.firebaseapp.com',
-  //     databaseURL: 'https://vikor-ce726.firebaseio.com',
-  //     projectId: 'vikor-ce726',
-  //     storageBucket: 'vikor-ce726.appspot.com',
-  //     messagingSenderId: '1010018308111',
-  //     appId: '1:1010018308111:web:37c880f06aad7cf4e1937a',
-  //     measurementId: 'G-HZBFL0F4LD',
-  //   }
-  //   if (!fire) {
-  //     setFire(firebase.initializeApp(firebaseConfig))
-  //   }
-  // })
   const [name, setName] = React.useState()
   const [email, setEmail] = React.useState()
   const [message, setMessage] = React.useState()
   const classes = useStyles()
   const submit = () => {
-    axios.post('http://localhost:3000/email', {
+    axios.post('https://bookryt.herokuapp.com/email', {
       subject: name + ' ' + email,
       body: message,
+      to: 'salman292013@gmail.com',
     })
   }
   return (
